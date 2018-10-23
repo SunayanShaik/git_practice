3e285650c9f877d79b03952b6b521c8834db0c57
Sunayan Shaik <sunayan.shaik001@umb.edu>
2018-10-22 21:48:47 -0400

    Story-1: Added the changes related to story 1

---
+++ Car.java
@@ -0,0 +1,41 @@
+package leetcode.questions.Strings;
+
+public class Car {
+
+	private String name;
+	private int year;
+	private String model;
+	
+	public Car(String name, int year, String model) {
+		this.name = name;
+		this.year = year;
+		this.model = model;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public String getModel() {
+		return model;
+	}
+
+	public void setModel(String model) {
+		this.model = model;
+	}
+	
+	
+	
+}
